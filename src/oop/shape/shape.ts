c5a7c5bb2f9ca8bac2ac867f452d3b87755c092f
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2023-09-17 23:30:19 +0330

    refactor: move shape classes to separate folder

--- src/oop/shape/shape.ts
+++ src/oop/shape/shape.ts
@@ -1,5 +1,12 @@
 export interface Shape {
+  /**
+   * Calculates the area of shape
+   */
   area(): number;
+
+  /**
+   * Calculates the perimeter of shape.
+   */
   perimeter(): number;
 }
 
@@ -27,6 +34,14 @@ export class Rectangle implements Shape {
   }
 }
 
+/**
+ * Calculates the area of given shape, no matter what shape
+ * you pass, because of the `Shape` interface guarantees
+ * that we have a `area()` method for each shape instance.
+ *
+ * @param shape Any shape you want to calculate it's area
+ * @returns Area of the given shape
+ */
 export function calculateTotalArea(shape: Shape): number {
   return shape.area();
 }
